9368d371774d3ecb3280ead6d5a00f994c87c8ae
KwaHash <cosmosray001@gmail.com>
2026-07-10 18:54:54 +0900

    testing

--- app/(main)/supporters/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import SupporterEditPage from '@/features/main/supporters/supporter-edit'
-
-export default function Page({ params }: { params: { id: string } }) {
-  return (
-    <SupporterEditPage id={params.id} />
-  )
-}
--- app/(main)/supporters/create/page.tsx
+++
@@ -1,7 +0,0 @@
-import SupporterCreatePage from '@/features/main/supporters/supporter-create'
-
-export default function Page() {
-  return (
-    <SupporterCreatePage />
-  )
-}
