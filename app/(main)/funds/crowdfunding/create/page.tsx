d5fc3c28197037dbac2463d21be357b4ed311bed
KwaHash <cosmosray001@gmail.com>
2026-07-30 18:36:05 +0900

    fix: crowdfunding-create url

---
+++ app/(main)/funds/crowdfunding/create/page.tsx
@@ -0,0 +1,5 @@
+import CrowdFundingCreatePage from '@/features/main/funds/crowdfunding-create'
+
+export default function Page() {
+  return <CrowdFundingCreatePage />
+}
--- app/(main)/funds/crowdfunding/page.tsx
+++
@@ -1,5 +0,0 @@
-import CrowdFundingPage from '@/features/main/funds/crowdfunding'
-
-export default function Page() {
-  return <CrowdFundingPage />
-}
